e8b91fcb91953d6f0f16c55212e9c70645f9d405
Omar El-Meteny <omar.elmeteny@gmail.com>
2022-12-21 19:49:55 +0200

    added mp3 player code

---
+++ mp3_player/mp3_player.ino
@@ -0,0 +1,127 @@
+/***************************************************
+DFPlayer - A Mini MP3 Player For Arduino
+ <https://www.dfrobot.com/product-1121.html>
+ 
+ ***************************************************
+ This example shows the basic function of library for DFPlayer.
+ 
+ Created 2016-12-07
+ By [Angelo qiao](Angelo.qiao@dfrobot.com)
+ 
+ GNU Lesser General Public License.
+ See <http://www.gnu.org/licenses/> for details.
+ All above must be included in any redistribution
+ ****************************************************/
+
+/***********Notice and Trouble shooting***************
+ 1.Connection and Diagram can be found here
+ <https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
+ 2.This code is tested on Arduino Uno, Leonardo, Mega boards.
+ ****************************************************/
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(11, 12); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+void setup()
+{
+  mySoftwareSerial.begin(9600);
+  Serial.begin(9600);
+  
+  Serial.println();
+  Serial.println("DFRobot DFPlayer Mini Demo");
+  Serial.println("Initializing DFPlayer ... (May take 3~5 seconds)");
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println("Unable to begin:");
+    Serial.println("1.Please recheck the connection!");
+    Serial.println("2.Please insert the SD card!");
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println("DFPlayer Mini online.");
+  
+  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+  myDFPlayer.play(1);  //Play the first mp3
+}
+
+void loop()
+{
+  static unsigned long timer = millis();
+  
+  if (millis() - timer > 3000) {
+    timer = millis();
+    myDFPlayer.next();  //Play next mp3 every 3 second.
+  }
+  
+  if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
+}
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println("Time Out!");
+      break;
+    case WrongStack:
+      Serial.println("Stack Wrong!");
+      break;
+    case DFPlayerCardInserted:
+      Serial.println("Card Inserted!");
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println("Card Removed!");
+      break;
+    case DFPlayerCardOnline:
+      Serial.println("Card Online!");
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print("Number:");
+      Serial.print(value);
+      Serial.println(" Play Finished!");
+      break;
+    case DFPlayerError:
+      Serial.print("DFPlayerError:");
+      switch (value) {
+        case Busy:
+          Serial.println("Card not found");
+          break;
+        case Sleeping:
+          Serial.println("Sleeping");
+          break;
+        case SerialWrongStack:
+          Serial.println("Get Wrong Stack");
+          break;
+        case CheckSumNotMatch:
+          Serial.println("Check Sum Not Match");
+          break;
+        case FileIndexOut:
+          Serial.println("File Index Out of Bound");
+          break;
+        case FileMismatch:
+          Serial.println("Cannot Find File");
+          break;
+        case Advertise:
+          Serial.println("In Advertise");
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+  
+}
