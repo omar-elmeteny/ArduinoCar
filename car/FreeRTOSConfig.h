f95e00678dc3585812fc4011dce2ca4500e47f97
Omar El-Meteny <omar.elmeteny@gmail.com>
2022-12-27 21:50:07 +0200

    add FreeRTOS config to car sketch

---
+++ car/FreeRTOSConfig.h
@@ -0,0 +1,9 @@
+#ifndef FreeRTOSConfig_h
+#define FreeRTOSConfig_h
+
+#define configUSE_TASK_NOTIFICATIONS 1
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES 1
+
+#define INCLUDE_vTaskDelay 1
+
+#endif
