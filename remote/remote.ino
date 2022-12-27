1239e780c0716b6057ec919bb54e644be2a92946
Omar El-Meteny <omar.elmeteny@gmail.com>
2022-12-27 20:41:02 +0200

    added bluetooth send command to car code

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,16 +1,3 @@
-/*
-  Showing numbers, chars and phrases
-                             A (seg[0] in this project)
-                            ---
-F (seg[5] in this project) |   | B (seg[1] in this project)
-                           |   | 
-                            --- G (seg[6] in this project)
-E (seg[4] in this project) |   | 
-                           |   | C (seg[2] in this project)
-                            ---  . dot or dicimal (seg[7] in this project)
-                             D (seg[3] in this project)
-
- */
 #include <Arduino_FreeRTOS.h>
 #include <SoftwareSerial.h>
 #include <limits.h>
@@ -24,8 +11,10 @@ char gearStatus = ' ';
 
 void joystickTask(void *pvParameters);
 void gearSevenSegmentTask(void *pvParameters);
+void bluetoothSendTask(void *pvParameters);
 
 TaskHandle_t gearSevenSegmentTaskHandle;
+TaskHandle_t bluetoothSendTaskHandle;
 
 void setup() {
   // uncomment to enable Serial.print
@@ -33,12 +22,23 @@ void setup() {
 
   setupSevenSegment();
   setupJoystick();
+  BTSerial.begin(38400);
 
   xTaskCreate(joystickTask, "JOYSTICK_TASK", 128, NULL, 1, NULL);
   xTaskCreate(gearSevenSegmentTask, "GEAR_7SEGMENT_TASK", 128, NULL, 2, &gearSevenSegmentTaskHandle);
+  xTaskCreate(bluetoothSendTask, "BLUETOOTH_SEND", 128, NULL, 2, &bluetoothSendTaskHandle);
 }
 
 void loop() {
+}
+
+void bluetoothSendTask(void *pvParameters) {
+  uint32_t command;
+  while (true) {
+    if (xTaskNotifyWait(ULONG_MAX, ULONG_MAX, &command, portMAX_DELAY) && command) {
+      BTSerial.write((char)command);
+    }
+  }  
 }
 
 void gearSevenSegmentTask(void *pvParameters) {
@@ -57,6 +57,7 @@ void joystickTask(void *pvParameters) {
     if (newGearStatus != gearStatus) {
       gearStatus = newGearStatus;
       xTaskNotify(gearSevenSegmentTaskHandle, (uint32_t)gearStatus, eSetValueWithOverwrite);
+      xTaskNotify(bluetoothSendTaskHandle, (uint32_t)gearStatus, eSetValueWithOverwrite);
     }
   }
 }
