4b3561f2124aa20b0d90fa7aa4e2df0e33947c90
Omar El-Meteny <omar.elmeteny@gmail.com>
2022-12-27 18:07:51 +0200

    added bluetooth module AT command code

---
+++ bluetooth_at/bluetooth_at.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial (10,11);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+  Serial.println("Enter AT Commands");
+  BTSerial.begin(38400);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(BTSerial.available()){
+    Serial.write(BTSerial.read());
+  }
+  if(Serial.available()){
+    BTSerial.write(Serial.read());
+  }
+}
